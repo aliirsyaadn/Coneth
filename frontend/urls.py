f297a46234e14c932b292463dea4014887dd7707
gafiirfandi <gafiirfandi45@gmail.com>
2020-12-30 16:13:58 +0700

    Adding post functions in views

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
-from .views import index
+from .views import index, rename, version, list_device, set_device, info, infonet, down, up, speed
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('rename', rename, name="rename"),
+    path('version', version, name="version"),
+    path('list_device', list_device, name="list_device"),
+    path('set_device', set_device, name="set_device"),
+    path('info', info, name="info"),
+    path('infonet', infonet, name="infonet"),
+    path('down', down, name="down"),
+    path('up', up, name="up"),
+    path('speed', speed, name="speed"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,78 @@
 from django.shortcuts import render
 
+import os
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def rename(request):
+
+    if request.method == "POST":
+        print("yey")
+        print(request.POST['current-name'])
+        print(request.POST['new-name'])
+
+        current_name = request.POST['current-name']
+        new_name = request.POST['new-name']
+        # print(os.system("bash conethz help"))
+        os.system('bash conethz rename ' + current_name + ' ' + new_name)
+
+
+
+    return render(request, 'index.html', {"hopTo": "rename"})
+
+def version(request):
+    if request.method == "POST":
+        print("yey")
+        os.system('bash conethz version')
+    return render(request, 'index.html')
+
+def list_device(request):
+    if request.method == "POST":
+        print("yey")
+        os.system('bash conethz list')
+    return render(request, 'index.html')
+
+def set_device(request):
+    device = request.POST['device']
+    speed = request.POST['speed']
+    duplex = request.POST['duplex']
+    auto_neg = request.POST['autoneg']
+    os.system(f'bash conethz set {device} {speed} {duplex} {autoneg}')
+    return render(request, 'index.html')
+
+def info(request):
+    if request.method == "POST":
+        print("yey")
+        device = request.POST['device']
+        os.system(f'bash conethz info {device}')
+    return render(request, 'index.html')
+
+def infonet(request):
+    if request.method == "POST":
+        print("yey")
+        device = request.POST['device']
+        os.system(f'bash conethz infonet {device}')
+    return render(request, 'index.html')
+
+def down(request):
+    if request.method == "POST":
+        print("yey")
+        device = request.POST['device']
+        os.system(f'bash conethz down {device}')
+    return render(request, 'index.html')
+
+def up(request):
+    if request.method == "POST":
+        print("yey")
+        device = request.POST['device']
+        os.system(f'bash conethz up {device}')
+    return render(request, 'index.html')
+
+def speed(request):
+    if request.method == "POST":
+        print("yey")
+        os.system('bash conethz speed')
+    return render(request, 'index.html')
+
